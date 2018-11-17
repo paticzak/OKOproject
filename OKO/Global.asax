f7dd2863bce4aeab74e1ab08ac61491cb9c38192
paticzak <p.mundzik@gmail.com>
2018-11-17 11:25:04 +0000

    Działaj

---
+++ OKO/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OKO.MvcApplication" Language="C#" %>
---
+++ OKO/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OKO.Startup))]
+namespace OKO
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
