f42bcb5670e67e7392be5fc9f5d35cd0b48fc8da
paticzak <p.mundzik@gmail.com>
2018-12-05 17:14:00 +0000

    Travel site cards working

---
+++ OKO/Controllers/OurTravelsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OKO.Controllers
+{
+    public class OurTravelsController : Controller
+    {
+        //
+        // GET: /OurTravels/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
