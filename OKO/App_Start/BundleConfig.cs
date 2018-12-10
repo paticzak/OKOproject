006c7620aa2da0a92c3bf3533734ff9a74db9067
paticzak <p.mundzik@gmail.com>
2018-12-10 23:01:38 +0000

    ALmost working gallery

--- OKO/App_Start/BundleConfig.cs
+++ OKO/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace OKO
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/lighbox-plus-jquery.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -25,7 +26,9 @@ namespace OKO
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/gallery.css",
+                      "~/Content/lightbox.min.css"));
         }
     }
 }
---
+++ OKO/Controllers/TravellingController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OKO.Controllers
+{
+    public class TravellingController : Controller
+    {
+        //
+        // GET: /Travelling/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Bieszczady()
+        {
+            return View();
+        }
+	}
+}
